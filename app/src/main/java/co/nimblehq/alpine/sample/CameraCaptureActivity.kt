9e7db7ccaaef3e86618fd991cd32fc88a86b13c5
Bruce - Thien Nguyen <thien@nimblehq.co>
2022-07-21 18:07:55 +0700

    #6 Integrate calling MRZ Processor to example activity

--- app/src/main/java/co/nimblehq/alpine/sample/CameraCaptureActivity.kt
+++ app/src/main/java/co/nimblehq/alpine/sample/CameraCaptureActivity.kt
@@ -2,27 +2,48 @@ package co.nimblehq.alpine.sample
 
 import android.app.Activity
 import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.camera.core.*
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.core.content.ContextCompat
+import androidx.lifecycle.lifecycleScope
+import co.nimblehq.alpine.R
 import co.nimblehq.alpine.databinding.ActivityCameraCaptureBinding
-import co.nimblehq.alpine.sample.extension.getStatusBarHeight
-import co.nimblehq.alpine.sample.extension.setLightStatusBar
-import java.lang.Exception
+import co.nimblehq.alpine.lib.model.MrzInfo
+import co.nimblehq.alpine.lib.mrz.*
+import co.nimblehq.alpine.sample.extension.*
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.launch
+import java.io.File
+import java.text.SimpleDateFormat
+import java.util.*
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraCaptureActivity : ComponentActivity() {
 
     private val binding: ActivityCameraCaptureBinding by lazy {
         ActivityCameraCaptureBinding.inflate(layoutInflater)
     }
+    private val photoCaptured: MutableStateFlow<File?> by lazy {
+        MutableStateFlow(null)
+    }
+    private val mrzProcessor: MrzProcessor by lazy {
+        MrzProcessor.newInstance()
+    }
 
     private var camera: Camera? = null
+    private var cameraProvider: ProcessCameraProvider? = null
     private var imageCapture: ImageCapture? = null
     private var preview: Preview? = null
-    private var cameraProvider: ProcessCameraProvider? = null
+
+    private lateinit var outputDirectory: File
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -30,6 +51,9 @@ class CameraCaptureActivity : ComponentActivity() {
         setLightStatusBar()
         setContentView(binding.root)
         setupView()
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+        outputDirectory = getMediaOutputDirectory()
     }
 
     private fun setupView() {
@@ -48,6 +72,8 @@ class CameraCaptureActivity : ComponentActivity() {
             }
 
             pvCameraCaptureViewFinder.post {
+                observeCapture()
+                updateCameraUi()
                 configureCamera()
             }
         }
@@ -85,10 +111,88 @@ class CameraCaptureActivity : ComponentActivity() {
         )
         // Attach the viewFinder's surface provider to Preview use case
         preview?.setSurfaceProvider(binding.pvCameraCaptureViewFinder.surfaceProvider)
+    }
 
+    private fun updateCameraUi() {
+        binding.ibCameraCapture.setOnClickListener {
+            imageCapture?.let {
+                val photoFile = createFile(outputDirectory)
+                val outputOptions = ImageCapture.OutputFileOptions.Builder(photoFile)
+                    .build()
+
+                // Setup image capture listener which is triggered after photo has been taken
+                it.takePicture(
+                    outputOptions,
+                    cameraExecutor,
+                    object : ImageCapture.OnImageSavedCallback {
+                        override fun onError(exc: ImageCaptureException) {
+                            showErrorMessage()
+                        }
+
+                        override fun onImageSaved(output: ImageCapture.OutputFileResults) {
+                            val savedUri = output.savedUri ?: Uri.fromFile(photoFile)
+                            notifyOnNewPhotoAdded(savedUri)
+                            photoCaptured.value = photoFile
+                        }
+                    })
+            }
+        }
+    }
+
+    private fun observeCapture() {
+        lifecycleScope.launch {
+            photoCaptured
+                .catch {
+                    showErrorMessage()
+                }
+                .collect { file ->
+                    file?.let {
+                        savePhotoUriAndNavigateBack(it)
+                    }
+                }
+        }
+    }
+
+    private fun savePhotoUriAndNavigateBack(photoFile: File) {
+        mrzProcessor.processImageFile(photoFile.absolutePath, object : MrzProcessorResultListener {
+            override fun onSuccess(mrzInfo: MrzInfo) {
+                Toast.makeText(
+                    this@CameraCaptureActivity,
+                    "Date of birth: ${mrzInfo.dateOfBirth}\n" +
+                            "Date of expired: ${mrzInfo.dateOfExpiry}\n" +
+                            "Document number: ${mrzInfo.documentNumber}",
+                    Toast.LENGTH_LONG
+                ).show()
+            }
+
+            override fun onError(e: MrzProcessorException) {
+                showErrorMessage()
+                Log.e(
+                    this@CameraCaptureActivity::class.java.canonicalName,
+                    "MRZ Processor process falied >>> $e"
+                )
+            }
+        })
+    }
+
+    private fun showErrorMessage() {
+        Toast.makeText(this, getString(R.string.error_generic), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun createFile(
+        baseFolder: File,
+        format: String = DATE_FORMAT_EXPORT_FILE_NAME,
+        extension: String = PHOTO_EXTENSION
+    ): File {
+        val child = SimpleDateFormat(format, Locale.getDefault())
+            .format(System.currentTimeMillis()) + extension
+        return File(baseFolder, child)
     }
 
     companion object {
+
+        private const val DATE_FORMAT_EXPORT_FILE_NAME = "yyyy-MM-dd-HH-mm-ss-SSS"
+        private const val PHOTO_EXTENSION = ".jpg"
 
         fun start(activity: Activity) {
             val intent = Intent(activity, CameraCaptureActivity::class.java)
--- app/src/main/java/co/nimblehq/alpine/sample/extension/ActivityExt.kt
+++ app/src/main/java/co/nimblehq/alpine/sample/extension/ActivityExt.kt
@@ -1,12 +1,23 @@
 package co.nimblehq.alpine.sample.extension
 
 import android.app.Activity
+import android.content.Intent
 import android.graphics.Color
+import android.hardware.Camera.ACTION_NEW_PICTURE
+import android.media.MediaScannerConnection
+import android.net.Uri
+import android.os.Build
+import android.util.Log
 import android.view.View
 import android.view.WindowManager
+import android.webkit.MimeTypeMap
+import androidx.core.net.toFile
+import co.nimblehq.alpine.R
+import java.io.File
 
 fun Activity.setLightStatusBar() {
-    window?.decorView?.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+    window?.decorView?.systemUiVisibility =
+        View.SYSTEM_UI_FLAG_LAYOUT_STABLE or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
     setWindowFlag(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS, false)
     window?.statusBarColor = Color.TRANSPARENT
 }
@@ -26,4 +37,33 @@ fun Activity.setWindowFlag(bits: Int, on: Boolean) {
 fun Activity.getStatusBarHeight(): Int {
     val resourceId = resources.getIdentifier("status_bar_height", "dimen", "android")
     return if (resourceId > 0) resources.getDimensionPixelSize(resourceId) else 0
+}
+
+fun Activity.getMediaOutputDirectory(): File {
+    val mediaDir = applicationContext.externalMediaDirs.firstOrNull()?.let {
+        File(it, applicationContext.resources.getString(R.string.app_name)).apply { mkdirs() }
+    }
+    return if (mediaDir != null && mediaDir.exists()) {
+        mediaDir
+    } else {
+        applicationContext.filesDir
+    }
+}
+
+fun Activity.notifyOnNewPhotoAdded(savedUri: Uri) {
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N) {
+        sendBroadcast(
+            Intent(ACTION_NEW_PICTURE, savedUri)
+        )
+    }
+
+    val mimeType = MimeTypeMap.getSingleton()
+        .getMimeTypeFromExtension(savedUri.toFile().extension)
+    MediaScannerConnection.scanFile(
+        this,
+        arrayOf(savedUri.toFile().absolutePath),
+        arrayOf(mimeType)
+    ) { _, uri ->
+        Log.d(this::class.java.canonicalName, "Video capture scanned into media store: $uri")
+    }
 }
