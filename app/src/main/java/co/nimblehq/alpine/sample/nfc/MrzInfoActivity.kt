627fdc0e9b9e3331900f54e60083d18c6fa69f74
Wadeewee <suphawadee@nimblehq.co>
2022-07-22 08:21:48 +0700

    [#11] Apply code from review

---
+++ app/src/main/java/co/nimblehq/alpine/sample/nfc/MrzInfoActivity.kt
@@ -0,0 +1,32 @@
+package co.nimblehq.alpine.sample.nfc
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import co.nimblehq.alpine.databinding.ActivityMrzInfoBinding
+
+class MrzInfoActivity : ComponentActivity() {
+
+    private val binding: ActivityMrzInfoBinding by lazy {
+        ActivityMrzInfoBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        setupView()
+    }
+
+    private fun setupView() {
+        with(binding) {
+
+            // TODO: Update on Integrate ticket.
+            val shouldEnabledNextButton = listOf(
+                passportNumber,
+                dateOfBirth,
+                dateOfExpiry
+            ).all { !it.editText?.text.isNullOrEmpty() }
+            nextButton.isEnabled = shouldEnabledNextButton
+        }
+    }
+}
