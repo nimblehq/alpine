0201db605bbb15980bdbba0d10eff697101f3982
Wadeewee <suphawadee@nimblehq.co>
2022-07-22 13:02:25 +0700

    [#12] Resolve conflict from develop

--- app/src/main/java/co/nimblehq/alpine/sample/MrzInfoActivity.kt
+++ app/src/main/java/co/nimblehq/alpine/sample/MrzInfoActivity.kt
@@ -4,7 +4,9 @@ import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
+import androidx.core.widget.doAfterTextChanged
 import co.nimblehq.alpine.databinding.ActivityMrzInfoBinding
+import co.nimblehq.alpine.lib.model.MrzInfo
 
 class MrzInfoActivity : ComponentActivity() {
 
@@ -12,24 +14,61 @@ class MrzInfoActivity : ComponentActivity() {
         ActivityMrzInfoBinding.inflate(layoutInflater)
     }
 
+    private var passportNumber: String = ""
+    private var dateOfBirth: String = ""
+    private var dateOfExpiry: String = ""
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
         setupView()
+        bindViewEvent()
     }
 
     private fun setupView() {
-        with(binding) {
+        enabledButtonNext()
+    }
 
-            // TODO: Update on Integrate ticket.
-            val shouldEnabledNextButton = listOf(
-                tilPassportNumber,
-                tilDateOfBirth,
-                tilDateOfExpiry
-            ).all { !it.editText?.text.isNullOrEmpty() }
-            btNext.isEnabled = shouldEnabledNextButton
+    private fun bindViewEvent() {
+        with(binding) {
+            tilPassportNumber.editText?.doAfterTextChanged {
+                passportNumber = it.toString()
+                enabledButtonNext()
+            }
+
+            tilDateOfBirth.editText?.doAfterTextChanged {
+                dateOfBirth = it.toString()
+                enabledButtonNext()
+            }
+
+            tilDateOfExpiry.editText?.doAfterTextChanged {
+                dateOfExpiry = it.toString()
+                enabledButtonNext()
+            }
+
+            btNext.setOnClickListener {
+                val mrzInfo = MrzInfo(
+                    documentNumber = this@MrzInfoActivity.passportNumber,
+                    dateOfBirth = this@MrzInfoActivity.dateOfBirth,
+                    dateOfExpiry = this@MrzInfoActivity.dateOfExpiry
+                )
+                navigateToNfcScanningScreen(mrzInfo)
+            }
         }
+    }
+
+    private fun enabledButtonNext() {
+        val shouldEnabledNextButton = listOf(
+            passportNumber,
+            dateOfBirth,
+            dateOfExpiry
+        ).all { it.isNotEmpty() }
+        binding.btNext.isEnabled = shouldEnabledNextButton
+    }
+
+    private fun navigateToNfcScanningScreen(mrzInfo: MrzInfo) {
+        NfcScanningActivity.start(this, mrzInfo)
     }
 
     companion object {
